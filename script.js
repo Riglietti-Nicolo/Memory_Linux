f72118e1801775ce68e0da4d00a37decd40116e0
Attanasio-Edoardo <edoardo.attanasio@istitutoagnelli.it>
2024-11-08 21:12:59 +0100

    modified index, style e js

---
+++ script.js
@@ -0,0 +1,58 @@
+const cardsArray = [
+    'images/ubuntu.png', 'images/zorint.png', 'images/arch.png', 'images/fedora.png',
+    'images/mint.png', 'images/garuda.png', 'images/puppy.png', 'images/kali.png',
+    'images/ubuntu.png', 'images/zorint.png', 'images/arch.png', 'images/fedora.png',
+    'images/mint.png', 'images/garuda.png', 'images/puppy.png', 'images/kali.png'
+];
+
+function returnObjectById(id){
+    return document.getElementById(id)
+}
+
+function createObject(oggetto){
+    return document.createElement(oggetto);
+}
+
+function shuffling(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+shuffling(cardsArray)
+
+function createCells(){
+
+    const container = returnObjectById("game_label");
+
+    cardsArray.forEach(distro => {
+        let divDistroImage =  createObject("div");
+        let distroImage = createObject("img");
+
+        distroImage.src = distro;
+
+        divDistroImage.classList.add("card");
+
+        divDistroImage.appendChild(distroImage);
+        container.appendChild(divDistroImage);
+
+        divDistroImage.addEventListener("click", () => {
+            flipCard(divDistroImage);
+        });
+    });
+}
+
+
+function flipCard(card){
+    if (card.classList.contains("hidden")) {
+        card.classList.remove("hidden"); 
+        card.classList.add("flipped");
+    } else {
+        card.classList.add("hidden");
+        card.classList.remove("flipped");
+    }
+}
+
+createCells();
